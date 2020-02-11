9b7210b330e4c05d31276b2972f49df317cf5cfa
Mehriban99 <57294601+Mehriban99@users.noreply.github.com>
2020-02-11 20:56:56 +0400

    Add files via upload

---
+++ ReqemOyunu/ReqemOyunu/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReqemOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int texmin=0, saygac=0;
+
+            Random myNum = new Random();
+            int reqem = myNum.Next(0, 100);
+
+            Console.WriteLine("Zehmet olmasa reqem daxil edin (0-100)");
+
+            while(texmin != reqem)
+            {
+                saygac++;
+                texmin = Convert.ToInt32(Console.ReadLine());
+                if(texmin > reqem)
+                {
+                    Console.WriteLine("Zehmet olmasa {0}-dan daha kicik reqem daxil edin", texmin);
+                }
+                else if(texmin < reqem) {
+                    Console.WriteLine("Zehmet olmasa {0}-dan daha boyuk reqem daxil edin", texmin);
+                }
+            }
+            Console.WriteLine("Tebrikler, siz {0} defeye dogru cavabi tapdiniz", saygac);
+            Console.ReadKey();
+        }
+    }
+}
